981c7929f6d9fff9c6e10cc768277907fe6c2a21
Onaopemipo Oluborode <opeoluborode@yahoo.com>
2024-01-21 17:27:38 +0100

    created a post endpoint for bookings to send data from the frontend to the table in airtable

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ import os
 from flask_cors import CORS
 from resources.article import Articles, Article
 from resources.comment import Comments
+from resources.booking import Bookings
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
 app.config['AIRTABLE_BASE_ID'] = os.environ.get('AIRTABLE_BASE_ID')
+app.config['BOOKING_AIRTABLE_BASE_ID'] = os.environ.get('BOOKING_AIRTABLE_BASE_ID')
 app.config['AIRTABLE_API_KEY'] = os.environ.get('AIRTABLE_API_KEY')
 
 api.add_resource(Articles, '/articles')
 api.add_resource(Comments, '/comments/<string:article_id>')
 api.add_resource(Article, '/articles/<string:article_id>')
+api.add_resource(Bookings, '/bookings')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ resources/booking.py
@@ -0,0 +1,30 @@
+from pyairtable import Table
+from flask import current_app, request, jsonify
+from flask_restful import Resource
+
+
+class Bookings(Resource):
+    def __init__(self):
+        self.base_id = current_app.config['BOOKING_AIRTABLE_BASE_ID']
+        self.api_key = current_app.config['AIRTABLE_API_KEY']
+        self.table_name = 'Bookings'
+        self.table = Table(self.api_key, self.base_id, self.table_name)
+
+    def post(self):
+        data = request.get_json()
+        record = {
+            "First Name": data.get('first_name'),
+            "Last Name": data.get('last_name'),
+            "Phone Number": data.get('phone_number'),
+            "Session Time": data.get('session_time'),
+            "How did you hear about us": data.get('how_did_you_hear_about_us'),
+            "Services": data.get('services'),
+            "Date": data.get('date')
+        }
+
+        response = self.table.create(record)
+
+        if response:
+            return {"message": "Booking created successfully!"}, 201
+        else:
+            return {"message": "Failed to create booking."}, 500
