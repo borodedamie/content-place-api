beb194448ca12f14dcad3f92ca0bbda774e2e379
Onaopemipo Oluborode <44114790+borodedamie@users.noreply.github.com>
2024-05-13 14:26:50 +0100

    Update article.py

--- resources/article.py
+++ resources/article.py
@@ -3,37 +3,6 @@ from flask import current_app, request
 from flask_restful import Resource
 from pyairtable import Table
 
-
-# class Articles(Resource):
-#     def get(self):
-#         base_id = current_app.config['AIRTABLE_BASE_ID']
-#         api_key = current_app.config['AIRTABLE_API_KEY']
-#         table_name = 'Articles'
-
-#         # Get the page size and offset from the request arguments
-#         page_size = request.args.get('page_size', 6)
-#         offset = request.args.get('offset')
-
-#         url = f'https://api.airtable.com/v0/{base_id}/{table_name}'
-#         headers = {'Authorization': f'Bearer {api_key}'}
-#         params = {'maxRecords': page_size, 'pageSize': page_size}
-
-#         if offset:
-#             params['offset'] = offset
-
-#         response = requests.get(url, headers=headers, params=params)
-#         data = response.json()
-
-#         records = data['records']
-#         next_offset = data.get('offset')
-
-#         result = {
-#             'records': records,
-#             'offset': next_offset
-#         }
-
-#         return result
-
 class Articles(Resource):
     def get(self):
         base_id = current_app.config['AIRTABLE_BASE_ID']
